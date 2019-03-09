9499b6be8f8b843b9118788f844789dd2c6c0d84
MSAIDI22 <malsaidi6@gmail.com>
2019-03-09 21:13:07 +0400

    TOS page added
    Corrected the content of the TOS page

--- src/views/TOS.vue
+++ src/views/TOS.vue
@@ -1,20 +1,22 @@
 <template>
   <body>
-    <nav class="navbar">
-      <span class="open-slide">
-        <a href="#" onclick="openSlideMenu()">
-          <i class="fas fa-bars fa-2x"></i>
-        </a>
-      </span>
-      <div class="logo" style="position: absolute; margin-left: 100px;" >
-              <img src="../assets/images/SPINlogowhite.png">
-          </div>
-    </nav>
-        <h2>About Us</h2>
-        <hr/>
-        <article style="line-height:30px;">
-          We are a company that aims to provide an easy access to bikes for renting that could either be used for many purposes such as commuting around the city, going for a mountain trail adventure, cruising along the beach and many more, with that being said, we provide different various types of bikes that support different environmental conditions for riding that can be booked as per desired.
-        </article>
-      <br/>
+    <div class="wrap" style="margin-top: 50px; width:600px;">
+      <h2 style="text-align:center;"> Terms of Service </h2>
+  <hr>
+  <article>Welcome to the terms and conditions regarding our company website and the services that we offer to you. By having access or using our website and proceeding towards account registration and using our bike rental service, you agree that you have read, understood and accept the Terms of Service and that you are accepting to follow our terms, policies and guidelines.
+    <p style="font-size: 13px; font-style:italic; color:red;">If you do not agree with out terms of service agreements including any necessary procedures, you should not use our website or service in any way or else otherwise.</p>
+</article>
+<br/>
+<article>
+
+    <h5 style="text-align:center;">Registration</h5>
+    To be able to use our rental system, you must first create an account to be used personally to allow the usage of our bikes through transactions. You will need to complete our registration forms that will include mandatory information about you as our client to be able to create your account. You are not able to access any transaction procedures without an account.
+    <h5 style="text-align:center;">Payement</h5>
+    You are to agree to our rate of charge and pay a full amount. We only accept credit card or debit cards for transaction payments. By successfully making a transaction, you agree to pay all fees and damage costs in case any incidents during the process of renting may arise. We do not provide any refunds at all.
+
+  </article>
+  <br/>
+  <br/>
+    </div>
   </body>
 </template>
