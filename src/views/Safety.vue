5237e7b28c58d81932cc6ca96a0f763d87c0cb38
MSAIDI22 <malsaidi6@gmail.com>
2019-03-12 21:50:37 +0400

    fixed indentation
    fixed indentation for safety vue file

--- src/views/Safety.vue
+++ src/views/Safety.vue
@@ -1,43 +1,40 @@
 <template>
   <body>
     <div class="wrap" style="margin-top: 50px; width:600px;">
-  <h2 style="text-align:center;"> Safety </h2>
-    <hr>
-    <article>
-      <img src="../assets/images/helmet.png" style="width:100px; height:100px; float:left; margin-left: 10px;">
+      <h2 style="text-align:center;"> Safety </h2>
+      <hr>
+      <article>
+         <img src="../assets/images/helmet.png" style="width:100px; height:100px; float:left; margin-left: 10px;">
+         <h4 style="font-weight:bold;">Wear a helmet!</h4>
+           Always wear a helmet at all cost for safety regardless the environment you will be cycling in.
+      </article>
+      <br/>
 
-      <h4 style="font-weight:bold;">Wear a helmet!</h4>
-      Always wear a helmet at all cost for safety regardless the environment you will be cycling in.
-    </article>
-    <br/>
-
-    <article>
-      <img src="../assets/images/seat.png" style="width:100px; height:100px; float:left; margin-left: -80px;">
-      <h4 style="font-weight:bold;">Inspect your bike!</h4>
-      Make sure to always check your bike to check if everything is in place to ensure maximum safety and comfort precautions.
-    </article>
-    <br/>
-    <article>
-      <ul style=" float:left;">
-        <li>Height of the seat</li>
-        <li>Wheels </li>
-        <li>Handle bar</li>
-        <li>The breaks</li>
-      </ul>
-    </article>
-    <br/>
-<br/>
-<br/>
-
-<br/>
-<br/>
-    <article>
-      <img src="../assets/images/road.png" style="width:100px; height:100px; ">
-      <h4 style="font-weight:bold;">Follow the traffic rules</h4>
-      Always abide by the rules on the road to avoid accidents or fines, check the laws in your current city concerning riding a bike such as from wearing a helmet to stopping at red lights. Cycle as if you are driving a vehicle, DO NO CYCLE IRRESPONSIBLY.
-    </article>
-
-    <br/>
-  </div>
+      <article>
+        <img src="../assets/images/seat.png" style="width:100px; height:100px; float:left; margin-left: -80px;">
+        <h4 style="font-weight:bold;">Inspect your bike!</h4>
+          Make sure to always check your bike to check if everything is in place to ensure maximum safety and comfort precautions.
+      </article>
+      <br/>
+      <article>
+         <ul style=" float:left;">
+           <li>Height of the seat</li>
+           <li>Wheels </li>
+           <li>Handle bar</li>
+           <li>The breaks</li>
+         </ul>
+      </article>
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+      <article>
+         <img src="../assets/images/road.png" style="width:100px; height:100px; ">
+         <h4 style="font-weight:bold;">Follow the traffic rules</h4>
+           Always abide by the rules on the road to avoid accidents or fines, check the laws in your current city concerning riding a bike such as from wearing a helmet to stopping at red lights. Cycle as if you are driving a vehicle, DO NO CYCLE IRRESPONSIBLY.
+      </article>
+      <br/>
+    </div>
   </body>
 </template>
