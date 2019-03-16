0c97e9b6d6cf3881f4325e3ee6ab6132f6af65b6
MSAIDI22 <malsaidi6@gmail.com>
2019-03-16 18:04:47 +0400

    updated placereservation vue

--- src/views/PlaceReservation.vue
+++ src/views/PlaceReservation.vue
@@ -15,11 +15,11 @@
 						<option value="5">5</option>
 				</b-form-select>
 		</div>
-					{{ selected }}
 			<br/>
 			<br/>
 			<h3><i class="fas fa-bicycle"></i> &nbsp; What type of bike? </h3>
 			<br/>
+			{{ selected }}
 			<div>
 				<div class="custom-control custom-radio" v-for="bikeType in this.$store.state.bikeTypes">
 					<BikeType v-bind:bikeTypeInfo="bikeType"></BikeType>
@@ -65,28 +65,65 @@
 					</div>
 				</div>
 	</div>
-</template>
+			<br/>
+			<br/>
+			<br/>
+			<h3><i class="fas fa-stopwatch"></i>&nbsp; What time? </h3>
+			<br/>
+			<span>
+				<h4 style="font-size:20px;">From: &nbsp;</h4>
+				<input class="form-control form-control-sm" style="width: 120px;" type="time" name="usr_time" required></span>
+			</form>
+			<br/>
+			<br/>
+			<hr>
+			<br/>
+				<div class="checkoutOption">
+				<div id="discard">
+					<a href='/'>
+						<button style="width:200px;" type="button" class="btn btn-danger">
+					Discard
+						</button>
+				</a>
+				</div>
+				<div id="makepayement">
+					<a href='/payement'>
+						<button style=" width:200px;" type="button" class="btn btn-success">
+					Book {{ this.$store.state.selectedBikeTypeId }}
+						&nbsp;<i class="fas fa-check fa-sm"></i>
+						</button>
+					</a>
+				</div>
+			</div>
+			<br/>
+			<br/>
+		</div>
+	 </body>
+	</template>
 
 <script>
 import BikeType from '../components/BikeType.vue'
 const fb = require('../firebaseConfig.js')
 
+// Make selection of timestamp from valid input element like celander
 export default {
 	components: {
 		BikeType
 	},
 	data () {
 		return {
-			selectedBikeTypeId: ''
-		}
-	},
-	methods: {
-		book() {
-			this.$store.commit('bookFirstAvailableBikeType',this.selectedBikeTypeId)
+			selected: '',
+			options: [
+				{ text: 'First radio', value: 'first' },
+				{ text: 'Second radio', value: 'second' },
+				{ text: 'Third radio', value: 'third' }
+			]
 		}
 	},
 	created () {
-		this.$store.dispatch('fetchbikeTypes')
+		if (this.$store.state.bikeTypes.length === 0) {
+			this.$store.dispatch('fetchbikeTypes')
+		}
 	}
 }
 </script>
