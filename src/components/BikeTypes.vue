3ce7960c6349318bb13901cb0a75776b7733e6ca
thedrhax14 <mishamrulla@gmail.com>
2019-03-20 17:53:02 +0400

    Update BikeTypes.vue

--- src/components/BikeTypes.vue
+++ src/components/BikeTypes.vue
@@ -71,7 +71,7 @@ export default {
 			return this.$store.state.selectedStation
 		},
 		AvailableBikeAtLocationByBikeTypeID() {
-
+			
 		},
 		FormTitle() {
 			return "Available book types at " + this.$store.state.selectedStation
