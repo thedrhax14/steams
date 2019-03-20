da5537ad899b1544574374c7f27b66246bb1e304
MSAIDI22 <malsaidi6@gmail.com>
2019-03-20 11:02:43 +0400

    Fixed card title in recents file
    Fixed card title in recents file

--- src/views/Recents.vue
+++ src/views/Recents.vue
@@ -1,5 +1,4 @@
 <template>
-  <body>
   <div class="wrap" style="margin-top: 50px;">
     <h2> Recent trips </h2>
     <hr>
@@ -7,7 +6,8 @@
     <div class="card mb-3">
 <div id='map' style='width: 500px; height: 180px;'></div>
   <div class="card-body">
-    <h5 class="card-title">Edinburgh Park</h5>
+    <h5>Edinburgh Park</h5>
+    <br/>
     <p class="card-text">
     <ul class="card-list">
       <li>Status: Complete</li>
@@ -17,33 +17,5 @@
     <p class="card-text"><small class="text-muted">19 hours ago</small></p>
   </div>
 </div>
-    <div class="card mb-3">
-  <div id='map' style='width: 500px; height: 180px;'></div>
-  <div class="card-body">
-    <h5 class="card-title">Royal Botanic Garden Edinburgh</h5>
-    <ul class="card-list">
-      <li>Status: Complete</li>
-      <li>Returned at: 12:24pm</li>
-      <li>Date: 1st April 2018</li>
-    </ul>
-    <p class="card-text"><small class="text-muted">3 days ago</small></p>
-  </div>
 </div>
-<div class="card mb-3">
-  <div id='map3' style='width: 500px; height: 180px;'></div>
-  <div class="card-body">
-    <h5 class="card-title">Edinburgh Mall</h5>
-    <ul class="card-list">
-      <li>Status: Cancelled</li>
-      <li>Returned at:4:55pm</li>
-      <li>Date: 23rd March 2018</li>
-    </ul>
-    <p class="card-text"><small class="text-muted"> 11 days ago</small></p>
-  </div>
-</div>
-   </div>
-<br/>
-<br/>
-
-  </body>
 </template>
