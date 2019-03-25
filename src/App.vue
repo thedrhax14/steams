99ab54bbc6b7ea9b522a551bfe99f8ea1c2781a5
thedrhax14 <mishamrulla@gmail.com>
2019-03-25 21:11:24 +0400

    Update App.vue
    Retricted navigation

--- src/App.vue
+++ src/App.vue
@@ -33,6 +33,7 @@
 			<router-link v-if='this.$store.state.userInfo.PermissionLevel == 2' to="/manStats" class="fas fa fa-chart-bar">
 				<span>Statistics</span>
 			</router-link>
+			<span @click='logout'>Log out</span>
 		</Push>
 		<main id="page-wrap">
 			<router-view/>
@@ -43,85 +44,12 @@
 <script>
 import LoadingDisplay from '@/components/LoadingDisplay.vue'
 import { Push } from 'vue-burger-menu'
-import { mapState } from 'vuex'
 const fb = require('./firebaseConfig.js')
 
 export default {
 	components: {
 		LoadingDisplay,
 		Push
-	},
-	computed: {
-		...mapState(['user']),
-		Menu() {
-			var menu = [
-				{
-					href: '/',
-					title: 'Map',
-					icon: 'fa fa-map'
-				}
-			]
-			if(this.$store.state.user) {
-				menu.push({
-					href: '/reservations',
-					title: 'Reservations',
-					icon: 'fa fa-book-open'
-				})
-				menu.push({
-					href: '/payement',
-					title: 'Payement',
-					icon: 'fa fa-credit-card'
-				})
-				if(this.$store.state.userInfo.PermissionLevel > 0) {
-					menu.push({
-						href: '/placeorders',
-						title: 'Place order',
-						icon: 'fa fa-bicycle'
-					})
-					menu.push({
-						href: '/revieworders',
-						title: 'Review orders',
-						icon: 'fa fa-th-list'
-					})
-					menu.push({
-						href: '/trackbl',
-						title: 'Track bike',
-						icon: 'fa fa-map-marked-alt'
-					})
-					if(this.$store.state.userInfo.PermissionLevel > 1) {
-						menu.push({
-							href: '/manStats',
-							title: 'Statistics',
-							icon: 'fa fa-chart-bar'
-						})
-					}
-					menu.push({
-						href: '/stationinterface',
-						title: 'Setup station',
-						icon: 'fa fa-wrench'
-					})
-				}
-			} else {
-				menu.push({
-					href: '/auth',
-					title: 'Sign in/up',
-					icon: 'fa fa-user'
-				})
-				menu.push({
-					href: '/reservations',
-					title: 'Reservations',
-					icon: 'fa fa-book-open',
-					disabled: true
-				})
-				menu.push({
-					href: '/payement',
-					title: 'Payement',
-					icon: 'fa fa-credit-card',
-					disabled: true
-				})
-			}
-			return menu
-		},
 	},
 	methods: {
 		logout () {
@@ -135,7 +63,10 @@ export default {
 	},
 	computed: {
 		IsAvailable(){
-			return !this.$store.state.isStationInterfaceActive
+			return !this.$store.state.isStationInterfaceActive && this.$store.state.user
+		},
+		IsLoggedIn(){
+			return this.$store.state.user
 		}
 	},
 	created () {
