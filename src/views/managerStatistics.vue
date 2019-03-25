3b6cb5b0d4b0c7c1abf50fd6805fd4f78371c667
SaKeAl <savannah.k.alphonso@gmail.com>
2019-03-25 14:23:56 +0400

    update

--- src/views/managerStatistics.vue
+++ src/views/managerStatistics.vue
@@ -1,51 +1,48 @@
-<template >
-  <div id = "manStats">
-    <div class="jumbotron" >
-      <div class="page-header">
-      <h1>Tojjjjday's Statistics</h1>
+<template>
+    <div v-if='datacollection' class="small">
+        <line-chart :chart-data="datacollection"></line-chart>
+        <button @click="fillData">Randomize</button>
     </div>
-    <hr>
-    </div>
-</div>
 </template>
 
-/*<canvas id="myChart" width="400" height="400"></canvas>
 <script>
-var ctx = document.getElementById('myChart');
-var myChart = new Chart(ctx, {
-    type: 'bar',
-    data: {
-        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-        datasets: [{
-            label: '# of Votes',
-            data: [12, 19, 3, 5, 2, 3],
-            backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
-            ],
-            borderColor: [
-                'rgba(255, 99, 132, 1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
-            ],
-            borderWidth: 1
-        }]
+import LineChart from './LineChart.js'
+
+export default {
+    components: {
+        LineChart
     },
-    options: {
-        scales: {
-            yAxes: [{
-                ticks: {
-                    beginAtZero: true
+    data () {
+        return {
+            datacollection: null
+        }
+    },
+    mounted () {
+        this.fillData()
+    },
+    methods: {
+        fillData () {
+            this.datacollection = {
+                labels: ['S', 'V'],
+                datasets: [
+                {
+                    label: 'Data One',
+                    backgroundColor: '#f87979',
+                    data: [5,6,7, 8]
                 }
-            }]
+                ]
+            }
+        },
+        getRandomInt () {
+            return Math.floor(Math.random() * (50 - 5 + 1)) + 5
         }
     }
-});
+}
 </script>
+
+<style>
+.small {
+    max-width: 600px;
+    margin:  150px auto;
+}
+</style>
