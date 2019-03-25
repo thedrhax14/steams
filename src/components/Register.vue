b6c83c0e0669f9496c937b6d3b86aee9da897784
MSAIDI22 <malsaidi6@gmail.com>
2019-03-25 20:11:43 +0400

    register

--- src/components/Register.vue
+++ src/components/Register.vue
@@ -1,117 +1,49 @@
 <template>
-	<!--<template>
-		<div class="sign-in-wrap">
-			<img src="../assets/images/SPINlogotext.png" alt="logo">
-			<div class="sign-in-window">
-			<h1> Sign In </h1>
+	<div >
+		<b-jumbotron  fluid>
+			<b-container>
+				<b-row>
+			  <h1>Let's get you started!</h1>
+			</b-row>
 			<br/>
 			<div class="form-group">
 				<div role="group">
-		    <label for="inputLive">Email address:</label>
-		    <b-form-input
-		      id="email1"
-		      v-model.trim="loginForm.email"
-		      trim
+				<label for="inputLive">Email address:</label>
+				<b-form-input
+					id="inputLive"
+					v-model.trim="loginForm.email"
+					trim
 					required="true"
-		      type="email"
-		      :state="emailState"
-		      aria-describedby="inputLiveHelp inputLiveFeedback"
-		      placeholder="you@email.com"
-		    />
+					type="email"
+					:state="emailState"
+					aria-describedby="inputLiveHelp inputLiveFeedback"
+					placeholder="you@email.com"
+				/>
+				<!-- This will only be shown if the preceeding input has an invalid state -->
 
-		    <label for="inputLive">Passsword:</label>
-		    <b-form-input
-		      id="email1"
+				<label for="inputLive">Passsword:</label>
+				<b-form-input
+					id="inputLive"
 					v-model.trim="loginForm.password"
-		      trim
+					trim
 					required="true"
-		      type="password"
-		      :state="passState"
-		      aria-describedby="inputLiveHelp inputLiveFeedback"
-		      placeholder="******"
-		    />
-
-		    <b-form-invalid-feedback id="inputLiveFeedback">
-		      Enter at least 8 letters
-		    </b-form-invalid-feedback>
-		  </div>
+					type="password"
+					:state="passState"
+					aria-describedby="inputLiveHelp inputLiveFeedback"
+					placeholder="******"
+				/>
+			</div>
 			</div>
 			<b-button @click="login" class="button">Log In</b-button>
 
-		</div>
-	</div>
-	</template>
--->
-	<div class="sign-in-wrap">
-		<label for="name">Full Name</label>
-		<input v-model.trim="signupForm.name" type="text" placeholder="user name" id="name" />
-		<label for="email2">Email</label>
-		<input v-model.trim="signupForm.email" type="text" placeholder="you@email.com" id="email2" />
-		<label for="password2">Password</label>
-		<input v-model.trim="signupForm.password" type="password" placeholder="min 6 characters" id="password2" />
-		<button @click="signup" class="button">Sign Up</button>
-		<b-row>
-		<h1>Let's get you started!</h1>
-	</b-row>
-	<div role="group">
-	<label for="inputLive">Full Name:</label>
-	<b-form-input
-		id="email1"
-		v-model.trim="signupForm.name"
-		trim
-		required="true"
-		type="name"
-		:state="null"
-		aria-describedby="inputLiveHelp inputLiveFeedback"
-		placeholder="you@email.com"
-	/>
-
-	<label for="inputLive">Email:</label>
-	<b-form-input
-		id="email1"
-		v-model.trim="signupForm.email"
-		trim
-		required="true"
-		type="password"
-		:state="emailState"
-		aria-describedby="inputLiveHelp inputLiveFeedback"
-		placeholder="you@email.com"
-	/>
-
-	<label for="inputLive">Passsword:</label>
-	<b-form-input
-		id="email1"
-		v-model.trim="signupForm.password"
-		trim
-		required="true"
-		type="password"
-		:state="passState"
-		aria-describedby="inputLiveHelp inputLiveFeedback"
-		placeholder="******"
-	/>
-
-	<b-form-invalid-feedback id="inputLiveFeedback">
-		Enter at least 8 letters
-	</b-form-invalid-feedback>
+		</b-container>
+	</b-jumbotron>
 </div>
-	</div>
 </template>
 
 <script>
 const fb = require('../firebaseConfig.js')
-
 export default {
-	name: 'Register',
-	data () {
-		return {
-			name:'',
-			signupForm: {
-				name: '',
-				email: '',
-				password: ''
-			}
-		}
-	},
 	computed:{
 		emailState() {
 			return this.loginForm.email.length > 5? true : null
@@ -119,28 +51,26 @@ export default {
 			return this.loginForm.password.length > 7? true : false
 		}
 	},
+	name: 'Login',
+	data () {
+		return {
+			name: '',
+			loginForm: {
+				email: '',
+				password: '',
+			}
+		}
+	},
 	methods: {
-		signup () {
+		login () {
 			this.$store.state.loading = true
-			fb.auth.createUserWithEmailAndPassword(this.signupForm.email, this.signupForm.password).then(user => {
-				this.$store.commit('updateUserProfile', {
-					displayName: this.signupForm.name
-				})
-				this.$store.commit('setCurrentUser', user.user)
-				fb.usersCollection.doc(user.user.uid).set({
-					PermissionLevel: 0,
-					Type: 'Customer',
-					PaymentMethods: []
-				}).then(() => {
-					this.$router.push('/profile')
-					this.$store.state.loading = false
-				}).catch(err => {
-					this.errorMsg = err.message
-					this.$store.state.loading = false
-				})
-			}).catch(err => {
-				this.errorMsg = err.message
+			fb.auth.signInWithEmailAndPassword(this.loginForm.email, this.loginForm.password).then(user => {
+				this.$store.commit('setUser', user.user)
+				this.$router.push('/')
 				this.$store.state.loading = false
+			}).catch(err => {
+				this.$store.state.loading = false
+				this.errorMsg = err.message
 			})
 		}
 	}
