4b18a3567884e35d045fd7b7d969b7873e27831a
SaKeAl <savannah.k.alphonso@gmail.com>
2019-03-25 08:50:20 +0400

    Update managerStatistics.vue

--- src/views/managerStatistics.vue
+++ src/views/managerStatistics.vue
@@ -1,14 +1,10 @@
-<template>
-	<div class="stats">
-  <body>
-      <div class="wrap" style="margin-top: 50px; width:600px;">
-        <h2>About Us</h2>
-          <hr/>
-           <article style="line-height:30px;">
-             We are a company that aims to provide an easy access to bikes for renting that could either be used for many purposes such as commuting around the city, going for a mountain trail adventure, cruising along the beach and many more, with that being said, we provide different various types of bikes that support different environmental conditions for riding that can be booked as per desired.
-           </article>
-         <br/>
-      </div>
-  </body>
-  </div>
+<template >
+  <div id = "manStats">
+    <div class="jumbotron" >
+      <div class="page-header">
+      <h1>Today's Statistics</h1>
+    </div>
+    <hr>
+    </div>
+</div>
 </template>
