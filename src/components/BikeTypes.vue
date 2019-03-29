228a809e55c0d38d6f32549c3e7306321e01ec08
MSAIDI22 <malsaidi6@gmail.com>
2019-03-29 22:01:52 +0400

    Bike type displayed instead of ID

--- src/components/BikeTypes.vue
+++ src/components/BikeTypes.vue
@@ -39,8 +39,11 @@
 			</b-row>
 			<div style="text-align:center;" class="booking-summary">
 				 <h4>Booking summary:</h4>
+				 <p> <i class="fas fa-credit-card"></i>&nbsp;
+				 Payment method:
+				 Card ending in ** {{ this.$store.state.userInfo.PaymentMethods[this.$store.state.userInfo.SelectedPaymentMethod]['Card number'].slice(14, 16) }} </p>
 				<p v-if='AvailableBikeAtLocationByBikeTypeID'>
-					Bike ID: {{ AvailableBikeAtLocationByBikeTypeID.id }}
+					Bike type: {{ BikeType.data['Type name'] }}
 				</p>
 				<p v-if='StartDate'>
 					Date: {{ StartDate }}
